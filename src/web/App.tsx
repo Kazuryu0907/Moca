e35fcbbe1e8ea9ed61ebbe2851fec600e2be1196
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2023-08-11 22:11:10 +0900

    Route化実装

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -4,18 +4,27 @@ import { Overlay } from "./components/Overlay";
 import { Teams } from "./components/Teams";
 import { Start } from "./components/Start";
 import { IdTable } from "./components/IdTable";
-import {MemoryRouter as Router,Routes,Route} from "react-router-dom";
+import {MemoryRouter as Router,Routes,Route,Navigate} from "react-router-dom";
+import { Main,Topper } from "./components/Main";
 
 export const App = () => {
   //Websocket更新時にState更新
-
+  console.log(window.location.pathname);
   return (
     <div>
       {/* Browserは内部にupdateもってるよ */}
+      {/* {useLocation().pathname !== "/" ?? <Topper/>} */}
         <Router>
           <Routes>
             <Route path="/" element={<Start/>} />
-            <Route path="/overlay" element={<div className="flex"><Teams/><IdTable/></div>}/>
+            {/* Topperと下のDynamicで分ける */}
+            <Route path="/overlay" element={<Topper/>}>
+              <Route index element={<Navigate to="/overlay/browser" replace/>}/>
+              <Route path="browser" element={<Browser/>}/>
+              <Route path="spread" element={<><Teams/><IdTable/></>}/>
+              <Route path="drive" element={<Overlay/>}/>
+              <Route path="*" element={<Navigate to="/overlay/browser" replace/>}/>
+            </Route>
           </Routes>
         </Router>
         {/* <Browser />
